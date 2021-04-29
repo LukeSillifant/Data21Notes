86f5ed56b8dac5ba4e5e2f8ecc3b22cbb26c2bf0
LukeSillifant <LSillifant@spartaglobal.com>
2021-04-29 23:38:36 +0100

    test update

--- testing code.py
+++ testing code.py
@@ -1,7 +1,25 @@
-a = tuple()
-b = (1, 2, 3)
-c = (2, 4, 1, 1)
-
-print(type(a))
-print(type(b))
-print(type(c))
+# a = tuple()
+# b = (1, 2, 3)
+# c = (2, 4, 1, 1)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+#
+# student_1 = {
+#     "name": "Luke",
+#     "stream": "data",
+#     "list": {"value1": 1, "value2": 1, "value3": 2}
+# }
+#
+# # print(student_1.keys())
+# # print([x for x in student_1])
+#
+# # print(student_1.values())
+# # print([student_1[x] for x in student_1])
+#
+# # print(student_1.items())
+# # print([[x, y] for x, y in student_1.items()])
+#
+# print(student_1)
+# print(student_1["list"]["value1"])
