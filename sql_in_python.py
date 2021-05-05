92c1fdb04075c5dac07acdfdaa00e56c909006fe
LukeSillifant <LSillifant@spartaglobal.com>
2021-05-05 10:39:08 +0100

    update

--- sql_in_python.py
+++ sql_in_python.py
@@ -4,6 +4,20 @@ server = 'localhost,1443'
 database = 'Northwind'
 user = 'SA'
 password = 'Passw0rd2018'
-driver = '0DCB+Driver+17+for+SQL+server'
+driver = 'SQL+Server'
 
 engine = sqlalchemy.create_engine(f"mssql+pyodbc://{user}:{password}@{server}/{database}?driver={driver}")
+
+connection = engine.connect()
+
+result = engine.execute('SELECT * FROM Products;')
+print(result)
+
+first_row = result.fetchone()
+print(first_row)
+
+all_results = result.fetchall()
+print(all_results)
+
+many_rows = result.fetchmany(10)
+print(many_rows)
